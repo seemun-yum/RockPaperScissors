3bee19ee027db9670b6c86ba7c7f3d4cf5951d6b
seemun69 <64670498+seemun69@users.noreply.github.com>
2021-11-11 01:07:51 +0800

    finished

---
+++ main.js
@@ -0,0 +1,44 @@
+function ComputerPlay(){
+    const choice=["Rock", "Paper", "Scissors"];
+    let random=Math.floor(Math.random()*choice.length);
+    console.log("Computer's choice: " +choice[random].toUpperCase());
+    return choice[random].toUpperCase();
+}
+
+
+function playRound(p,c){
+    if ((p=="ROCK" && c=="PAPER")|| (p=="PAPER" && c=="SCISSORS") || (p=="SCISSORS" && c=="ROCK")){
+        console.log("Player Lose");
+        return 0; 
+    }
+    else if (p==c){
+        console.log("tie");
+        return 0;
+    }
+    else {
+        console.log("Player Wins !!");
+        return 1;
+    }
+}
+
+
+
+function game(){
+    let playerScore=0;
+    for (let i=1; i<=5; i++){
+        let playerSelect=prompt("Enter choice (rock, paper, or scissors");
+        let playerSelection=playerSelect.toUpperCase(); 
+        console.log("Player: "+playerSelection);
+        let computerSelection=ComputerPlay(); 
+        
+        let score=playRound(playerSelection,computerSelection);
+
+        if (score==1){
+            playerScore=playerScore+1;
+        }
+
+    }
+    console.log("Player Score: "+ playerScore);
+}
+
+game();
